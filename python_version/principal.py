4bfeed4535e5b0473045b297d8f75af0ec1f27ae
javier <jalcantarac97@gmail.com>
2026-01-14 12:56:40 +0100

    Trabajando en la clase principal.

--- python_version/principal.py
+++ python_version/principal.py
@@ -3,21 +3,19 @@ from aventura import Aventura
 
 RUTA_BD = './traveller.db'
 
-class Principal(object):
+class Modelo(object):
     def __init__(self):
         self.aventura = None
         self.base_datos = GestorBase(RUTA_BD)
     def nueva_aventura(self, nombre, fecha_estelar, narrador=None):
         aventura = Aventura(nombre, fecha_estelar, narrador)
         av_id = self.base_datos.nueva_aventura(aventura)
+        aventura.establecer_id(av_id)
         bienes = self.base_datos.cargar_bienes(av_id)
         reglas = self.base_datos.cargar_reglas(av_id)
         aventura.cargar_bienes(bienes)
         aventura.cargar_reglas(reglas)
-    def cargar_aventura(self, nombre):
-        self.aventura = self.base_datos.cargar_aventura(nombre)
-    def mostrar_aventuras(self):
-        aventuras = self.base_datos.mostrar_aventuras()
+    def mostrar_aventuras(self)
         l = []
         for aventura in aventuras:
             l.append(aventura["nombre"])
@@ -44,6 +42,8 @@ class Principal(object):
         l = self.base_datos.mostrar_flotas_por_aventura(av_id)
         for flota in l:
             self.base_datos.borrar_flota(flota["id"])
-    def crear_flota(self, nombre):
+    def crear_flota(self, nombre, mayor_rango_marina, mayor_rango_explorador, mayor_rango_broker, mayor_rango_socializar, mayor_rango_callejeo, mayor_bono_soc):
+        flota = Flota(nombre, mayor_rango_marina, mayor_rango_explorador, mayor_rango_broker, mayor_rango_socializar, mayor_rango_callejeo, mayor_bono_soc)
+        self.base_datos.nueva_flota(flota, av_id)
         
         
