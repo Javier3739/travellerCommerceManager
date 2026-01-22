033daf2d2db18ebde6180b2037ed0f84e4c02d36
Javi <jalcantarac97@gmail.com>
2026-01-22 18:20:28 +0100

    Avanzando en la creación de la API

--- python_version/adventureNotSelectedError.py
+++
@@ -1,6 +0,0 @@
-class AdventureNotSelectedError(Exception):
-    def __init__(self, msg=""):
-        self.args = msg
-    def __str__(self):
-        return ("Aventura no seleccionada. Debes seleccionar una para realizar esta acción. "
- + str(self.args))
